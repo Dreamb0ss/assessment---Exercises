3897630d9fb95f79b2f06a1edb93e957b7defdc7
Muhammad Basil <99414688+Dreamb0ss@users.noreply.github.com>
2025-12-31 11:25:34 +0300

    Some Counting.py
    Some Counting.py

---
+++ Excerises 07/Some Counting.py
@@ -0,0 +1,19 @@
+for i in range(0, 51):
+    print(i)  
+    # prints numbers from 0 to 50
+
+for i in range(50, -1, -1):
+    print(i)  
+    # prints numbers from 50 down to 0
+
+for i in range(30, 51):
+    print(i)  
+    # prints numbers from 30 to 50
+
+for i in range(50, 9, -2):
+    print(i)  
+    # prints even numbers from 50 down to 10
+
+for i in range(100, 201, 5):
+    print(i)  
+    # prints numbers from 100 to 200 in steps of 5
