0ee212a6b57f7a9f4f6ce762f37eb806341ca83c
Muhammad Basil <99414688+Dreamb0ss@users.noreply.github.com>
2025-12-31 11:22:25 +0300

    Primitive Quiz.py
    Primitive Quiz.py

---
+++ Excercises 04/Primitive Quiz.py
@@ -0,0 +1,11 @@
+answer = input("What is the capital of France? ").lower()  
+# asks the user a question and converts input to lowercase
+
+if answer == "paris":  
+    # checks if the answer is correct
+    print("Correct!")  
+    # prints message for correct answer
+else:
+    print("Wrong!")  
+    # prints message for wrong answer
+
