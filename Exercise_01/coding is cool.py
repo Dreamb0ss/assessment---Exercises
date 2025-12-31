e805c3420d51039dda9882b80bcfbb774f3fa65b
Muhammad Basil <99414688+Dreamb0ss@users.noreply.github.com>
2025-12-31 11:42:59 +0300

    coding is cool.py
    coding is cool.py

--- Exercise_01/coding is cool.py
+++ Exercise_01/coding is cool.py
@@ -2,4 +2,4 @@ word1 = "Coding"
 word2 = "is"
 word3 = "Cool"
 
-print(word1 + " " + word2 + " " + word3) #using print command to print coding is cool.
+print(word1 + " " + word2 + " " + word3)  #using print command to print coding is cool.
